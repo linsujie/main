1e2ffec836cb34a1545515b28c07702a80430357
linsj <lin.sujie1234@gmail.com>
2015-04-01 14:30:46 +0800

    20150401

--- unix/replace-my-vim.sh
+++ unix/replace-my-vim.sh
@@ -4,6 +4,8 @@ cp ./dist/ctags_lang ~/.ctags
 cp .vimrc ~/.vimrc
 cp .vimrc.plugins ~/.vimrc.plugins
 cp .vimrc.local ~/.vimrc.local
+cp .vimrc.tex ~/.vimrc.tex
+cp .vimrc.map ~/.vimrc.map
 cp .vimrc.plugins.local ~/.vimrc.plugins.local
 rm -rf ~/.vim
 cp -r vimfiles ~/.vim
